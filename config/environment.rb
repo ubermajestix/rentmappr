8c1e500683494c75a569ab85da6f7cd34b60c341
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2009-08-04 15:37:48 -0600

    hpricot update

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,7 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
-  config.gem "hpricot", :version => '0.8', :source => "http://code.whytheluckystiff.net"
+  config.gem "hpricot"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'mislav-will_paginate', :version => '~> 2.3.8', :lib => 'will_paginate', 
