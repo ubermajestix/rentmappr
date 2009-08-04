7e46a8676121a0d33f66fe0ad13e839feefd782d
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2009-08-04 15:44:14 -0600

    removing memcache dependency

--- config/environment.rb
+++ config/environment.rb
@@ -19,10 +19,10 @@ Rails::Initializer.run do |config|
     :expires    =>  2*86400, 
     :secret      => 'a31d361ea2fa93b75a194e021b730e0a57c2be96e1fd9376dbf60a7c21847118189d146052487cab980e9a66ed2c17dc5a95bb01f47c9b43782eebbf58d377b0'
   }
-  require 'memcache'
-
-    config.action_controller.session_store = :mem_cache_store
-    CACHE = MemCache.new 'localhost:11211', :namespace => 'rentmappr'
+  # require 'memcache'
+  # 
+  #   config.action_controller.session_store = :mem_cache_store
+  #   CACHE = MemCache.new 'localhost:11211', :namespace => 'rentmappr'
 
 
   # Add additional load paths for your own custom dirs
