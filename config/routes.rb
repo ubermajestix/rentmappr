143eca65d101dea79ada83b2b287c01e4884670a
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2009-08-07 17:28:02 -0600

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ ActionController::Routing::Routes.draw do |map|
   
   map.edit_area '/map_areas/edit/:id', :controller=>"map_areas", :action=>"edit"
 
-  map.list_houses '/houses/:id', :controller => "houses", :action => "list"
+  map.list_houses '/list/:id', :controller => "houses", :action => "list"
   
    map.confirm        '/reset_password/:reset_code',                    :controller => 'users',  :action => 'reset_password'
   # The priority is based upon order of creation: first created -> highest priority.
