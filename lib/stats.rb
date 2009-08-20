a4106c3d061eb8b8f3a6c35babb4a3e5164c356f
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2009-08-20 15:43:43 -0600

    updated stats to use better faster strong sql queries instead of loops

--- lib/stats.rb
+++ lib/stats.rb
@@ -3,7 +3,7 @@
 # require ''
 # Dir.glob("app/models/*.rb").sort.each {|rb| require rb}
 # 
-
+require 'jabber_logger'
 
 module Stats
   def self.initialize
@@ -18,8 +18,7 @@ module Stats
     output << last_12_hours if opts[:last_12]
     output << week  if opts[:week]
     
-
-
+    JabberLogger.send output.flatten.join(opts[:format] || "\n")
     return output.flatten.join(opts[:format] || "\n")
   end
   
@@ -39,33 +38,45 @@ module Stats
   
   def self.areas
     output = []
-    map_areas = MapArea.find(:all)
-    for area in map_areas
-      houses = area.houses
-      output << "---#{area.name}---"
-      output << fspt(houses)
+    # count houses for all areas, group by area
+    houses=House.find_by_sql("select count(*) as count, map_area_id, geocoded from houses group by map_area_id,geocoded")
+    for set in houses      
+      output << "---#{set.map_area.name}---"
+      output << "#{set.geocoded}: #{set.count}"
     end
     return output
   end
   
   def self.week
     output = []
-     7.times do |day| 
-      houses = House.find(:all, :conditions => ["created_at <= ? and created_at >= ?",Time.now - day.days,  Time.now - (day+1).days])# 
-      output << "---#{(Time.now - day.days).strftime('%m/%d/%y')}---"
-      output << fspt(houses)
+    houses = House.find_by_sql("SELECT date_trunc('day', created_at) AS day, count(*) AS count, map_area_id, geocoded FROM houses WHERE created_at > now() - interval '1 week' GROUP BY map_area_id, day, geocoded  ORDER BY map_area_id, day, geocoded")
+    #  7.times do |day| 
+    #   houses = House.find(:all, :conditions => ["created_at <= ? and created_at >= ?",Time.now - day.days,  Time.now - (day+1).days])# 
+    #   output << "---#{(Time.now - day.days).strftime('%m/%d/%y')}---"
+    #   output << fspt(houses)
+    # end
+    for set in houses
+      output << "---#{set.map_area.name}---"
+      output << "#{set.day}"
+      output << "#{set.geocoded}: #{set.count}"
     end
     return output
   end
   
   def self.last_12_hours
     output = []
-    12.times do |hour|
-      hour-=1
-      houses = House.find(:all, :conditions => ["created_at <= ? and created_at >= ?",Time.now - hour.hours, Time.now - (hour+1).hours]) 
-      output << "--- #{(Time.now - hour.hours).strftime('%H:%M')} ---"
-      output << fspt(houses)
+    houses = House.find_by_sql("SELECT date_trunc('hour', created_at) AS hour, count(*) AS count, map_area_id, geocoded FROM houses WHERE created_at > now() - interval '12 hours' GROUP BY map_area_id, hour, geocoded  ORDER BY map_area_id, hour, geocoded")
+    for set in houses
+      output << "---#{set.map_area.name}---"
+      output << "#{set.hour}"
+      output << "#{set.geocoded}: #{set.count}"
     end
+    # 12.times do |hour|
+    #   hour-=1
+    #   houses = House.find(:all, :conditions => ["created_at <= ? and created_at >= ?",Time.now - hour.hours, Time.now - (hour+1).hours]) 
+    #   output << "--- #{(Time.now - hour.hours).strftime('%H:%M')} ---"
+    #   output << fspt(houses)
+    # end
     return output
   end
 end
