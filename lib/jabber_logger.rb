812244d94bb549e9df1d71f7c6375de545b89250
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2009-08-20 15:16:19 -0600

    form fix

--- lib/jabber_logger.rb
+++ lib/jabber_logger.rb
@@ -9,12 +9,12 @@ module JabberLogger
     @client = Client::new(jid)
     @client.connect('talk.google.com', 5222)
     @client.auth(password)
-    @client.allow_tls = true
-    @client.send(Jabber::Presence.new.set_status("rentmappr logging say what?"))
+    # @client.allow_tls = true
+    @client.send(Jabber::Presence.new.set_status("ready for logging say what?"))
     to = JID::new("tyler.a.montgomery@gmail.com")
     subject = "Logging Output"
     body = message.to_s
-    m = Message::new(to, body)#.set_type(:normal).set_id('1').set_subject(subject)
+    m = Message::new(to, body).set_type(:chat).set_id('1').set_subject(subject)
     puts m.inspect
     @client.send m
   end
