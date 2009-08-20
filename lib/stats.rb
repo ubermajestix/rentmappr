00c7ac7bc5f28169bc2d589696b882ab84df4e21
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2009-08-20 16:28:43 -0600

    stats bugs

--- lib/stats.rb
+++ lib/stats.rb
@@ -2,10 +2,10 @@ module Stats
   def self.initialize
   end
   def self.get_stats(opts={})
-    @houses = House.find_by_sql("select count(*) as count, geocoded from houses group by geocoded")
+    @houses = House.find_by_sql("select count(*) as count from houses group by geocoded")
     output = []
     output << Time.now.to_s 
-    @houses.each{|set| output << "#{set.geocoded}: #{set.count}"}
+    @houses.each{|set| output << "Total Collected: #{set.count}"}
 
     output << areas if opts[:areas]
     output << last_12_hours if opts[:last_12]
@@ -20,9 +20,9 @@ module Stats
     map_areas.each do |map_area|
       days = {}
       m=houses.select{|h| h.map_area_id == map_area.id}
-      m.collect{|d| d.time}.uniq.reverse.each{|day| days[day]=m.select{|d| d.time==day}} 
+      m.collect{|d| d.time}.uniq.each{|day| days[day]=m.select{|d| d.time==day}} 
       output << "---#{map_area.name}---"
-      days.each_pair{|day, obj| output << "--#{day}--"; obj.each{|set| output << "#{set.geocoded}: #{set.count}"}}
+      days.each_pair{|day, obj| obj.each{|set| output << "#{set.day}: #{set.count}"}}
     end
     return output
   end
@@ -32,17 +32,17 @@ module Stats
     output = []
     houses=House.find_by_sql("select count(*) as count, map_area_id, geocoded from houses group by map_area_id,geocoded")
     houses.collect{|h| h.map_area.name}.uniq.each{|area| areas[area]=houses.select{|a| a.map_area.name == area }}
-    areas.each_pair{|area, obj| output << "---#{area}---"; obj.each{|set| output << "#{set.geocoded}: #{set.count}" }}
+    areas.each_pair{|area, obj| obj.each{|set| output << "#{set.map_area.name}: #{set.count}" }}
     return output
   end
   
   def self.week
-    houses = House.find_by_sql("SELECT date_trunc('day', created_at) AS time, count(*) AS count, map_area_id, geocoded FROM houses WHERE created_at > now() - interval '1 week' GROUP BY map_area_id, time, geocoded  ORDER BY map_area_id, time, geocoded")
+    houses = House.find_by_sql("SELECT date_trunc('day', created_at) AS time, count(*) AS count, map_area_id FROM houses WHERE created_at > now() - interval '1 week' GROUP BY map_area_id, time  ORDER BY map_area_id, time")
     return time_output(houses)
   end
   
   def self.last_12_hours
-    houses = House.find_by_sql("SELECT date_trunc('hour', created_at) AS time, count(*) AS count, map_area_id, geocoded FROM houses WHERE created_at > now() - interval '12 hours' GROUP BY map_area_id, time, geocoded  ORDER BY map_area_id, time, geocoded")
+    houses = House.find_by_sql("SELECT date_trunc('hour', created_at) AS time, count(*) AS count, map_area_id FROM houses WHERE created_at > now() - interval '12 hours' GROUP BY map_area_id, time,  ORDER BY map_area_id, time")
     return time_output(houses)
   end
 end
